ca9a22c734f6d1e61da70e8179f59495663f14a3
Dicky arya Kesuma <dickyaryakesuma@gmail.com>
2020-09-13 20:30:18 +0700

    Update main.go

--- main.go
+++ main.go
@@ -24,50 +24,6 @@ func hello(c echo.Context) error {
 	return c.String(http.StatusOK, fmt.Sprintf("Hello %s, your are authorized", session.Name))
 }
 
-//func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
-//	return func(c echo.Context) error {
-//		c.Response().Header().Set(echo.HeaderServer, "Server-1.0")
-//		return next(c)
-//	}
-//}
-
-
-//func Middleware() echo.MiddlewareFunc {
-//	return func(handler echo.HandlerFunc) echo.HandlerFunc {
-//		return func(c echo.Context) error {
-//			//	get token from cookie
-//			cookie, err := c.Cookie("JWTCookie")
-//			//if cookie doesnt exist
-//			if err != nil {
-//				c.String(http.StatusUnauthorized, "Not Authorized")
-//				return handler(c)
-//			}
-//			//	get jwt token
-//			tokenString := cookie.Value
-//
-//			claims := &JwtClaims{}
-//
-//			token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-//				return []byte("secret key"), nil
-//			})
-//
-//			if err != nil {
-//				if err == jwt.ErrSignatureInvalid {
-//					c.String(http.StatusUnauthorized, "Not Authorized")
-//					return handler(c)
-//				}
-//				c.String(http.StatusBadRequest, "Something is wrong")
-//				return handler(c)
-//			}
-//			if !token.Valid {
-//				c.String(http.StatusUnauthorized, "Not Authorized")
-//				return handler(c)
-//			}
-//
-//			return handler(c)
-//		}
-//	}
-//}
 
 func main() {
 	e := echo.New()
